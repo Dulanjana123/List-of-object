3875d65c91a8d34a2ee4c6ec39b7d497dbfd51a1
Dulanjana <Dulanjana@DESKTOP-4MSIP85>
2022-11-03 22:34:50 +0530

    Add project files.

---
+++ List of object/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    public class Author
+    {
+        public string name { get; set; }
+        public short age { get; set; }
+        public string title { get; set; }
+        public bool mvp { get; set; }
+
+
+    public Author(string name, short age, string title, bool mvp)
+        {
+            this.name = name;
+            this.age = age;
+            this.title = title;
+            this.mvp = mvp;
+        }
+
+    }
+
---
+++ List of object/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_of_object
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //The following code snippet creates a List of Author type.
+            List<Author> Authorlist = new List<Author>();
+
+            //The following code snippet creates the Author objects and adds them to the List.
+            Authorlist.Add ( new Author ("Mahesh Chand", 35, "A Prorammer's Guide to ADO.NET", true) );
+            Authorlist.Add(new Author("Neel Beniwal", 18, "Graphics Development with C#", false));
+
+            //The following code snippets loop through the List and prints out all the items of the List.
+            foreach (var author in Authorlist)
+            {
+                Console.WriteLine("Author: {0},{1},{2},{3}", author.name, author.age, author.title, author.mvp);
+            }   
+        }
+    }
+}
